fee868b138875f0f9124d4a3ea281ae7f4ca00b6
Seohyun Park <ge48soc@tum.de>
2022-11-14 17:54:08 +0100

    add

--- Simulations/MassSpringSystemSimulator.h
+++ Simulations/MassSpringSystemSimulator.h
@@ -26,14 +26,14 @@ public:
 // Do Not Change
 
 
-class MassSpringSystemSimulator :public Simulator {
+class MassSpringSystemSimulator:public Simulator{
 public:
 	// Construtors
 	MassSpringSystemSimulator();
-
+	
 	// UI Functions
-	const char* getTestCasesStr();
-	void initUI(DrawingUtilitiesClass* DUC);
+	const char * getTestCasesStr();
+	void initUI(DrawingUtilitiesClass * DUC);
 	void reset();
 	void drawFrame(ID3D11DeviceContext* pd3dImmediateContext);
 	void notifyCaseChanged(int testCase);
@@ -41,6 +41,7 @@ public:
 	void simulateTimestep(float timeStep);
 	void onClick(int x, int y);
 	void onMouse(int x, int y);
+	const char* getIntegratorStr();
 
 	// Specific Functions
 	void setMass(float mass);
